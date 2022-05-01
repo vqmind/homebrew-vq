552b137a289f54bf2863a4d67aec75c143eb5d5d
vqmind <102002862+vqmind@users.noreply.github.com>
2022-05-01 00:31:19 -0400

    Create vq.rb

---
+++ vq.rb
@@ -0,0 +1,13 @@
+require "formula"
+ 
+class Srcclr < Formula
+  homepage "https://vqmind.github.io/vq/"
+  url "https://github.com/vqmind/vq/releases/download/1.0.0/app-1.0.0.tar.gz"
+  sha256 "057d570bd3a9522f6e392c3a5dbbf412d4cc676b16f752626b9acdc06438f3d3"
+ 
+  def install
+      inreplace "brew/vq", "##PREFIX##", "#{prefix}"
+      prefix.install "app-1.0.0.jar"
+      bin.install "brew/vq"
+  end
+end
