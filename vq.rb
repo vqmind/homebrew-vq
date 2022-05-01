45ceffaafd75ad86441b204955779d9be974667c
vqmind <102002862+vqmind@users.noreply.github.com>
2022-05-01 01:03:26 -0400

    Update vq.rb

--- vq.rb
+++ vq.rb
@@ -1,7 +1,6 @@
-require "formula"
- 
 class Vq < Formula
   homepage "https://vqmind.github.io/vq/"
+  desc "Utility for querying json."
   url "https://github.com/vqmind/vq/releases/download/1.0.0/app-1.0.0.tar.gz"
   sha256 "057d570bd3a9522f6e392c3a5dbbf412d4cc676b16f752626b9acdc06438f3d3"
   license "MIT"
@@ -10,5 +9,9 @@ class Vq < Formula
       inreplace "brew/vq", "##PREFIX##", "#{prefix}"
       prefix.install "app-1.0.0.jar"
       bin.install "brew/vq"
+  end
+ 
+  test do
+      assert_match "seattle", shell_output(" echo '{\"city\":\"seattle\"}' | #{bin}/vq \"$.city\" ")
   end
 end
