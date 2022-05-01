9a7f4540aa5fadcce1236509c67981962fc717c0
vqmind <102002862+vqmind@users.noreply.github.com>
2022-05-01 00:50:49 -0400

    Update vq.rb

--- vq.rb
+++ vq.rb
@@ -5,7 +5,7 @@ class Vq < Formula
   url "https://github.com/vqmind/vq/releases/download/1.0.0/app-1.0.0.tar.gz"
   sha256 "057d570bd3a9522f6e392c3a5dbbf412d4cc676b16f752626b9acdc06438f3d3"
   license "MIT"
-  depends_on :java => "8"
+  depends_on :java
  
   def install
       inreplace "brew/vq", "##PREFIX##", "#{prefix}"
