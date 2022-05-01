e3f251c3ff333248d4b1a26e740f8cc88a89799d
vqmind <102002862+vqmind@users.noreply.github.com>
2022-05-01 01:09:08 -0400

    Update vq.rb

--- vq.rb
+++ vq.rb
@@ -1,5 +1,5 @@
 class Vq < Formula
-  desc "Utility for querying json."
+  desc "Utility for querying json"
   homepage "https://vqmind.github.io/vq/"
   url "https://github.com/vqmind/vq/releases/download/1.0.0/app-1.0.0.tar.gz"
   sha256 "057d570bd3a9522f6e392c3a5dbbf412d4cc676b16f752626b9acdc06438f3d3"
