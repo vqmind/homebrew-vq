1a7dbaa9558d75703ef0155a3b3bd785b055e0c4
vqmind <102002862+vqmind@users.noreply.github.com>
2022-05-01 00:34:03 -0400

    Update vq.rb

--- vq.rb
+++ vq.rb
@@ -1,6 +1,6 @@
 require "formula"
  
-class Srcclr < Formula
+class Vq < Formula
   homepage "https://vqmind.github.io/vq/"
   url "https://github.com/vqmind/vq/releases/download/1.0.0/app-1.0.0.tar.gz"
   sha256 "057d570bd3a9522f6e392c3a5dbbf412d4cc676b16f752626b9acdc06438f3d3"
